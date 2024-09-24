5b8471402b2021e045647c332ccde2f576430e7c
Lux <iansseijelly@berkeley.edu>
2024-09-24 16:27:38 -0700

    ADD: using object + capstone, successfully parsed instructions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 extern crate clap;
-extern crate elf;
+extern crate object;
+extern crate capstone;
 
+use std::fs::File;
+use std::io::Read;
 use clap::Parser;
-use elf::{ElfStream};
-use elf::endian::AnyEndian;
-use elf::to_str::{e_machine_to_human_str, e_osabi_to_string, e_type_to_human_str};
+use capstone::prelude::*;
+use capstone::arch::riscv::{ArchMode, ArchExtraMode};
+use object::{Object, ObjectSection};
+
 
 #[derive(Parser)]
 #[command(name = "trace-decoder", version = "0.1.0", about = "Decode trace files")]
@@ -17,50 +21,41 @@ struct Args {
     verbose: bool,
 }
 
-fn print_file_header(ehdr: &elf::file::FileHeader<AnyEndian>) {
-    let e_type_str = match e_type_to_human_str(ehdr.e_type) {
-        Some(s) => s.to_string(),
-        None => format!("e_type({:#x})", ehdr.e_type),
-    };
+fn parse_elf(file_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = File::open(file_name)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
 
-    let e_machine_str = match e_machine_to_human_str(ehdr.e_machine) {
-        Some(s) => s.to_string(),
-        None => format!("e_machine({:#x})", ehdr.e_machine),
-    };
+    let elf = object::File::parse(&*buffer)?;
+    
+    // assert this is for 64 bit RISC-V
+    assert!(elf.architecture() == object::Architecture::Riscv64);
 
-    println!("File Header:");
-    println!("  Class: {:?}", ehdr.class);
-    println!("  Endianness: {:?}", ehdr.endianness);
-    println!("  Object Type: {e_type_str}");
-    println!("  Arch: {e_machine_str}");
-    println!("  OSABI: {}", e_osabi_to_string(ehdr.osabi));
-    println!("  Entry point address: {:#x}", ehdr.e_entry);
-    println!(
-        "  Start of program headers: {:#x} (bytes into file)",
-        ehdr.e_phoff
-    );
-    println!(
-        "  Start of section headers: {:#x} (bytes into file)",
-        ehdr.e_shoff
-    );
-    println!("  Flags: {:#x}", ehdr.e_flags);
-    println!("  Size of this header: {:#x}", ehdr.e_ehsize);
-    println!("  Size of program header: {:#x}", ehdr.e_phentsize);
-    println!("  Number of program headers: {:#x}", ehdr.e_phnum);
-    println!("  Size of section header: {:#x}", ehdr.e_shentsize);
-    println!("  Number of section headers: {:#x}", ehdr.e_shnum);
-    println!(
-        "  Section headers string table section index: {}",
-        ehdr.e_shstrndx
-    );
+    // Find the .text section (where the executable code resides)
+    let text_section = elf.section_by_name(".text").ok_or("No .text section found")?;
+    let text_data = text_section.data()?;
+    let entry_point = elf.entry();
+    
+    let cs = Capstone::new()
+        .riscv()
+        .mode(ArchMode::RiscV64)
+        .extra_mode([ArchExtraMode::RiscVC].iter().copied())
+        .detail(true)
+        .build()?;
+
+    let decoded_instructions = cs.disasm_all(&text_data, entry_point).unwrap();
+    println!("found {} instructions", decoded_instructions.len());
+
+    Ok(())
 }
 
 fn main() {
     let args = Args::parse();
 
-    let path = std::path::PathBuf::from(args.elf);
-    let io = std::fs::File::open(path).expect("Could not open file");
-    let elf_file = ElfStream::<AnyEndian, _>::open_stream(io).expect("Failed to open ELF stream");
+    let mut file = File::open(args.elf.clone()).expect("Failed to open ELF file");
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).expect("Failed to read ELF file");
 
-    print_file_header(&elf_file.ehdr);
+    parse_elf(&args.elf).expect("Failed to parse ELF file");
+
 }
